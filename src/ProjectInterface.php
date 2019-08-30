7dbc6c23c7a3e5d8c9ad68da285b005a92a45218
nguyenanhung <info@nguyenanhung.com>
2019-08-30 09:34:29 +0700

    Cập nhật hằng số version

--- src/ProjectInterface.php
+++ src/ProjectInterface.php
@@ -22,8 +22,8 @@ interface ProjectInterface
     /**
      * Base version of Project
      */
-    const VERSION       = '1.0.3';
-    const LAST_MODIFIED = '2019-07-21';
+    const VERSION       = '1.0.4';
+    const LAST_MODIFIED = '2019-08-30';
     const AUTHOR_NAME   = 'Hung Nguyen';
     const AUTHOR_EMAIL  = 'dev@nguyenanhung.com';
     const PROJECT_NAME  = 'My Cache';
