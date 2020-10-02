ffc905f87f16b8cdc42da2127f84640bb11c4ad5
nguyenanhung <info@nguyenanhung.com>
2020-10-03 01:34:49 +0700

    Update Phpfastcache

--- src/ProjectInterface.php
+++ src/ProjectInterface.php
@@ -19,8 +19,8 @@ namespace nguyenanhung\MyCache;
  */
 interface ProjectInterface
 {
-    const VERSION       = '2.0.0';
-    const LAST_MODIFIED = '2020-10-02';
+    const VERSION       = '2.0.1';
+    const LAST_MODIFIED = '2020-10-03';
     const AUTHOR_NAME   = 'Hung Nguyen';
     const AUTHOR_EMAIL  = 'dev@nguyenanhung.com';
     const PROJECT_NAME  = 'My Cache';
